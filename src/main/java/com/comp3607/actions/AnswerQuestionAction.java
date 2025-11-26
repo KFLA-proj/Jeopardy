2c76cbffa7de97b23373628c3650805ff77e3129
Kareem Farrell <kareemfarrellonline@gmail.com>
2025-11-26 16:45:37 -0400

    Update game actions with logging

--- src/main/java/com/comp3607/actions/AnswerQuestionAction.java
+++ src/main/java/com/comp3607/actions/AnswerQuestionAction.java
@@ -3,8 +3,9 @@ package com.comp3607.actions;
 import com.comp3607.core_game.Player;
 import com.comp3607.questions.Question;
 import com.comp3607.core_game.Game;
+import com.comp3607.logger.GameLogger;
 
-public class AnswerQuestionAction implements ActionTaken{
+public class AnswerQuestionAction implements ActionTaken {
     private Player player;
     private Question question;
     private char answer;
@@ -18,10 +19,25 @@ public class AnswerQuestionAction implements ActionTaken{
     @Override
     public void execute(Game game){
         player.recordAction(this);
-        if (question.getAnswer() == answer){
+
+        boolean correct = question.getAnswer() == answer;
+        String result;
+        if (correct) {
+            result = "Correct";
+        } else {
+            result = "Incorrect";
+        }
+
+        if (correct){
             player.awardPoints(question.getValue());
             game.getVault().removeQuestion(question);
             game.dropActiveQuestion();
+        }
+
+        for (var obs : game.getObservers()) {
+            if (obs instanceof GameLogger logger) {
+                logger.logTurn(player, "Answer Question", question, String.valueOf(answer), result);
+            }
         }
     }
 
--- src/main/java/com/comp3607/actions/SelectQuestionAction.java
+++ src/main/java/com/comp3607/actions/SelectQuestionAction.java
@@ -3,10 +3,11 @@ package com.comp3607.actions;
 import com.comp3607.core_game.Player;
 import com.comp3607.questions.Question;
 import com.comp3607.core_game.Game;
-//import com.comp3607.questions.Vault;
+import com.comp3607.core_game.GameObserver;
+import com.comp3607.logger.GameLogger;
 
 
-public class SelectQuestionAction implements ActionTaken{
+public class SelectQuestionAction implements ActionTaken {
     private Player player;
     private Question q;
 
@@ -16,15 +17,20 @@ public class SelectQuestionAction implements ActionTaken{
     }
 
     @Override
-    public void execute(Game game){
+    public void execute(Game game) {
         player.recordAction(this);
         game.setActiveQuestion(q);
         q.displayOptions();
-        
+
+        for (GameObserver obs : game.getObservers()) {
+            if (obs instanceof GameLogger logger) {
+                logger.logTurn(player, "Select Question", q, null, null);
+            }
+        }
     }
 
     @Override
-    public String info(){
+    public String info() {
         return player.getName() + " selected question: " + q.toString() + ".";
     }
 }
